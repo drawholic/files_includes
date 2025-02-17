1446f36701f57114fb089c8afa5fa59b72c600c5
drawholka <mamrokhavlad@gmail.com>
2025-02-17 14:27:19 +0200

    FileNode init to check

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-
-
 #include "Preprocessor.hpp"
-
+#include "FileNode.hpp"
 
 int main(int argc, char const *argv[])
-{
-	/* code */
+{ 
+	if(argc < 2)
+	{
+		std::cout << "Usage: ./main [input file]\n";
+		exit(0);
+	};
+
+	FileNode fn(argv[1]);
+	fn.read_file();
+	fn.print(); 
+	fn.include_files();
+
 	return 0;
 }
